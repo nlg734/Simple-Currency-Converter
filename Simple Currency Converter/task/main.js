ccfdf2de72f4cac7ebf6ba674898dde1a2d4d2e5
Natasha Graham <nlgraham95@gmail.com>
2022-06-03 09:32:18 -0800

    Initial Commit. Stage 1/4 complete.

---
+++ Simple Currency Converter/task/main.js
@@ -0,0 +1,19 @@
+/* main.js
+ * Simple Currency Calculator
+ * Written by Natasha Graham
+ * 2022-05-24
+ */
+
+
+usd = 1.0;
+jpy = 113.5;
+eur = 0.89;
+rub = 74.36;
+gbp = 0.75;
+
+console.log("Welcome to Currency Converter!");
+console.log(`1 USD equals  ${usd} USD
+1 USD equals  ${jpy} JPY
+1 USD equals  ${eur} EUR
+1 USD equals  ${rub} RUB
+1 USD equals  ${gbp} GBP`);
