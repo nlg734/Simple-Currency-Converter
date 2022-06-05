085e248d9441851c7b4aff07182ace5d07cbf991
Natasha Graham <nlgraham95@gmail.com>
2022-06-05 14:28:44 -0800

    Stage 2/4 complete. Completes actual currency conversion.

--- Simple Currency Converter/task/main.js
+++ Simple Currency Converter/task/main.js
@@ -5,15 +5,31 @@
  */
 
 
-usd = 1.0;
-jpy = 113.5;
-eur = 0.89;
-rub = 74.36;
-gbp = 0.75;
+let currencies = {"USD": 1.0, "JPY": 113.5, "EUR": 0.89, "RUB": 74.36, "GBP": 0.75};
 
 console.log("Welcome to Currency Converter!");
-console.log(`1 USD equals  ${usd} USD
-1 USD equals  ${jpy} JPY
-1 USD equals  ${eur} EUR
-1 USD equals  ${rub} RUB
-1 USD equals  ${gbp} GBP`);
+console.log(`1 USD equals  ${currencies["USD"]} USD
+1 USD equals  ${currencies["JPY"]} JPY
+1 USD equals  ${currencies["EUR"]} EUR
+1 USD equals  ${currencies["RUB"]} RUB
+1 USD equals  ${currencies["GBP"]} GBP
+I can convert USD to these currencies: JPY, EUR, RUB, USD, GBP`);
+
+let curr = prompt("Type the currency you wish to convert: USD To: ");
+if(!currencies.includes(curr.toUpperCase())) {
+    console.log("Unknown currency");
+}
+else {
+    let amt = prompt("Amount: ");
+    amt = Number(amt)
+    if(amt !== amt) {
+        console.log("The amount has to be a number")
+    }
+    else if (amt < 1) {
+        console.log("The amount cannot be less than 1")
+    }
+    else {
+        let res = amt * currencies[curr];
+        console.log(`Result: ${amt} USD equals ${res} ${curr.toUpperCase()}`);
+    }
+}
